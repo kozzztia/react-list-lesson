eeb04e3be4afa749f875583152aea67839a4e9fd
kozzztia <kozzztia@gmail.com>
2024-10-03 20:19:06 +0300

    finish + done some harold

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,9 +1,24 @@
 import styles from "./styles.module.css"
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome, faLink } from "@fortawesome/free-solid-svg-icons"
+import harold from "../assets/harold-starik.jpg"
 
 const Reviews = () => {
   return (
     <div className={styles.page}>
-        Reviews
+      <figure >
+        <img src={harold} alt="user" />
+        <figcaption>
+          <h3>Welcome to react-admin demo</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non doloribus totam quidem eius repudiandae. Sit est iste voluptatem adipisci repellendus suscipit odio ipsum. Assumenda, eaque!
+          </p>
+          <span>
+            <Link to="#"><FontAwesomeIcon icon={faHome} />React-admin-site</Link>
+            <Link to="#"><FontAwesomeIcon icon={faLink} />Sourxe for this demo</Link>
+          </span>
+        </figcaption>
+      </figure>
     </div>
   )
 }
--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,9 +1,24 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
+import harold from "../assets/harold.jpg";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faLink } from '@fortawesome/free-solid-svg-icons';
 
 const Sales = () => {
   return (
     <div className={styles.page}>
-        Sales
+      <figure >
+        <img src={harold} alt="user" />
+        <figcaption>
+          <h3>Welcome to react-admin demo</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non doloribus totam quidem eius repudiandae. Sit est iste voluptatem adipisci repellendus suscipit odio ipsum. Assumenda, eaque!
+          </p>
+          <span>
+            <Link to="#"><FontAwesomeIcon icon={faHome} />React-admin-site</Link>
+            <Link to="#"><FontAwesomeIcon icon={faLink} />Sourxe for this demo</Link>
+          </span>
+        </figcaption>
+      </figure>
     </div>
   )
 }
